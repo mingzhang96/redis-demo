fe1adc683690918457d7a6518fa24d5fc02f8b59
zhangming <389157423@qq.com>
2018-01-12 19:53:13 +0800

    添加了单独启动和杀死某端口redis进程, 发布订阅断线重连

--- bin/redis-tool.sh
+++ bin/redis-tool.sh
@@ -9,23 +9,23 @@
 # └── 7031
 #     └── redis.conf
 #
-# startnodes 启动所有节点
+# startnodes 启动所有节点 [portNumber]启动对应端口的节点
 # startcluster [--localhost]启动本地集群 [--outside]将本地节点添加到已有集群中
-# kill 关闭所有redis节点
+# kill [all]关闭所有redis节点 [portNumber]关闭对应端口的节点
 # check 检查集群状态
 
 # 本机ip地址
-localIP=192.168.1.61
+localIP=192.168.1.225
 # 安装集群的根目录
 rootPath=~/WorkSpace/redis-cluster-test/redis-cluster
 # redis的安装路径
-redisPath=/usr/local/Cellar/redis/redis
+redisPath=/usr/local/Cellar/redis/redis-3.2.11/
 # 单台机器节点总数
-nodes=32
+nodes=8
 # 从这个端口数开始
 startPort=7000
 # master个数，不能超过单台机器总节点数的一半，默认剩下的都是slave
-master=14
+master=3
 # 集群中已经存在的redis节点，用于在另外一台机器上部署的节点加入现有集群
 clusterNowAddr=192.168.1.61:7000
 
@@ -41,7 +41,7 @@ if [ ! -d $rootPath/db ]; then
 fi
 
 if [[ $1 = "create" ]]; then
-	for i in `seq 0 $nodesCount` 
+	for i in `seq 0 $nodesCount`
 	do
 	    nowPort=`expr $startPort + $i`
 		if [ ! -d ${rootPath}"/"${nowPort} ]; then
@@ -63,6 +63,10 @@ if [[ $1 = "create" ]]; then
 	    echo "finish ${localIP}:${nowPort}"
 	done
 	echo "create dir structure successfully!"
+
+elif [[ $1 = "startnodes" ]] && [[ -n $2 ]]; then
+	nohup ${redisPath}/src/redis-server ${rootPath}/${2}/redis.conf >> ${rootPath}/${2}/redis-${2}.log 2>&1 &
+	echo "startnode : ${2}"
 
 elif [[ $1 = "startnodes" ]]; then
 	for i in `seq 0 $nodesCount`
@@ -125,11 +129,25 @@ elif [[ $1 = "startcluster" ]] && [[ $2 = "--outside" ]]; then
 	${tribRebalance}
 	echo "add to cluster successfully!"
 
-elif [[ $1 = "kill" ]]; then
+elif [[ $1 = "kill" ]] && [[ $2 = "all" ]]; then
 	# centos
-	ret=`ps -fe | grep python | awk '{print $2}' | xargs -i  kill -s 9 {}`
+	# ret=`ps -fe | grep redis-server | awk '{print $2}' | xargs -i  kill -s 9 {}`
 	# macOS
-	# ret=`ps -fe | grep redis | awk '{print $2}' | xargs -I p kill "p"`
+	ret=`ps -ef | grep redis-server | awk '{print $2}' | xargs -I p kill "p"`
+	if [[ $ret -ne 0 ]]; then
+		echo "kill failed!"
+		exit
+	else
+		echo "kill successfully!"
+	fi
+
+elif [[ $1 = "kill" ]] && [[ -n $2 ]]; then
+	# centos
+	# ret=`ps -ef | grep "redis-server ${localIP}:$2" | awk '{print $2}' | xargs -i  kill -s 9 {}`
+	# macOS
+	echo $2
+	echo "ps -ef | grep \"redis-server ${localIP}:$2\" | awk '{print \$2}' | xargs -I p kill \"p\""
+	ret=`ps -ef | grep "redis-server ${localIP}:$2" | awk '{print $2}' | xargs -I p kill "p"`
 	if [[ $ret -ne 0 ]]; then
 		echo "kill failed!"
 		exit
@@ -146,19 +164,25 @@ elif [ -z $1 ]; then
 	echo -e "Usage: ./redis-tool.sh <command> <options>"
 	echo -e "  create          to create base dir structure"
 	echo -e "  startnodes      to start all nodes up"
+	echo -e "      <portNumber>      to start portNumber locally"
 	echo -e "  startcluster    to start cluster & set master and slave"
-	echo -e "      --localhost     to start cluster locally"
-	echo -e "      --outside       to add nodes to other ip and start cluster"
-    echo -e "  kill            to kill all redis process"
+	echo -e "      <--localhost>     to start cluster locally"
+	echo -e "      <--outside>       to add nodes to other ip and start cluster"
+    echo -e "  kill"
+    echo -e "      <all>             to kill all redis process"
+	echo -e "      <portNumber>      to kill portNumber process"
     echo -e "  check           to check cluster's state"
 else
 	echo -e "Usage: ./redis-tool.sh <command> <options>"
 	echo -e "  create          to create base dir structure"
 	echo -e "  startnodes      to start all nodes up"
+	echo -e "      <portNumber>      to start portNumber locally"
 	echo -e "  startcluster    to start cluster & set master and slave"
-	echo -e "      --localhost     to start cluster locally"
-	echo -e "      --outside       to add nodes to other ip and start cluster"
-    echo -e "  kill            to kill all redis process"
+	echo -e "      <--localhost>     to start cluster locally"
+	echo -e "      <--outside>       to add nodes to other ip and start cluster"
+    echo -e "  kill"
+    echo -e "      <all>             to kill all redis process"
+	echo -e "      <portNumber>      to kill portNumber process"
     echo -e "  check           to check cluster's state"
 fi
 
--- src/test/java/cn/zmhappy/redisdemo/RedisDemoApplicationTests.java
+++ src/test/java/cn/zmhappy/redisdemo/RedisDemoApplicationTests.java
@@ -1,11 +1,13 @@
 package cn.zmhappy.redisdemo;
 
+import cn.zmhappy.redisdemo.jedis.JedisClusterConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import redis.clients.jedis.JedisCluster;
+import redis.clients.jedis.JedisPubSub;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -21,7 +23,45 @@ public class RedisDemoApplicationTests {
 	@Test
 	public void jedisTest() {
 		jedisCluster.set("faceall2", "bb");
+	}
 
+//	private String channel_name = "Selected_Camera_Channel";
+    private String channel_name = "zhangming";
+//    private String channel_name = "selectedCameraIds";
+
+    @Test
+	public void serverTest() {
+		try {
+			for (int i = 0; i < 1000000; i++) {
+				jedisCluster.publish(channel_name, i+"");
+				Thread.sleep(200);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void clientTest() {
+	    while (true) {
+	        try {
+                jedisCluster.subscribe(new JedisPubSub() {
+                    @Override
+                    public void onMessage(String channel, String message) {
+                        System.out.println("getting message from " + channel + " : " + message);
+                    }
+
+                    @Override
+                    public void onSubscribe(String channel, int subscribedChannels) {
+                        System.out.println("subscribe successfully!");
+                    }
+                }, channel_name);
+            } catch (Exception e) {
+//                jedisCluster = new JedisClusterConfig().jedisCluster();
+                e.printStackTrace();
+            }
+
+        }
 	}
 
 }
